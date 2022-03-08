08caf2842b2930f3331022109b5bd1545239c562
Chris Williams <spider.mane.web@gmail.com>
2022-03-08 15:29:40 -0500

    `maybe_define_abspath` helper function added

--- src/functions.php
+++ src/functions.php
@@ -6,3 +6,10 @@ function abspath(string $dir): string
 {
     return $dir . '/' . WpCliUtil::getInstallPath() . '/';
 }
+
+function maybe_define_abspath(string $dir): void
+{
+    if (!defined('ABSPATH')) {
+        define('ABSPATH', abspath($dir));
+    }
+}
